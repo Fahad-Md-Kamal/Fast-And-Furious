ce621b2924ed8854e747c504dacbaf272642f795
fahad-md-kamal <faahad.hossain@gmail.com>
2022-02-25 11:19:29 +0600

    - Added BaseModel for Default datatype of the post

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from typing import Optional
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class Post(BaseModel):
+    """ Responsible for maping Post Object's Data field """
+    title: str
+    content: str
+    published: bool = True # Assigning Default Value
+    rating: Optional[int] = None # Completely Optional Field
 
 @app.get("/", tags=['POST'])
 async def home_view():
     return {"message": "Bismiallahir Rahmanir Rahim"}
 
-@app.post("/create-post", tags=['POST'])
-async def create_post(payload: dict=Body(...)):
 
-    return {"new_post" : f"title: {payload['title']} content: {payload['content']}"}
-    
+@app.post("/create-post", tags=['POST'])
+async def create_post(post:Post):
+    print(post.dict())
+    return {"data":"Post Created Successfully"}
