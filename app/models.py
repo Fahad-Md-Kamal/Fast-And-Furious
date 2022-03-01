2a6b74abfc5f826a4947e975530e765023682d37
fahad-md-kamal <faahad.hossain@gmail.com>
2022-03-01 14:11:38 +0600

    Database missing field added

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
-
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = "posts"
@@ -9,5 +10,6 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, default=True)
-    
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
     
